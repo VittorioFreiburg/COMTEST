9de4c6fc69340a4e923e3374c908e5acb477e5ec
VittorioLippi <vittorio.lippi@tu-berlin.de>
2020-11-06 17:35:12 +0100

    Sin funcitions
    some more indexes from Lippi, Vittorio, et al. "COMTEST Project: A Complete Modular Test Stand for Human and Humanoid Posture Control and Balance." 2019 IEEE-RAS 19th International Conference on Humanoid Robots (Humanoids). IEEE, 2019.

--- ANALYSIS CODE/OCTAVE/RCinfo.m
+++ ANALYSIS CODE/OCTAVE/RCinfo.m
@@ -1,13 +1,17 @@
 function s=RCinfo(y,t)
-  setvalue=y(end);
-  peakIdx=find(x==max(x),1,'first');
+  setValue=y(end);
+  error=abs(y-setValue)/setValue;
+  setlingIdx=find(error>0.02,1,'last');
+  s.setlingTime=t(min(length(t),setlingIdx+1));
+  peakIdx=find(y==max(y),1,'first');
   s.peak=t(peakIdx);
-  s.setlingMax=max(x);
-  s.setlingMin=min(x(s.peak:end))
-  s.overshoot=((s.setlingMax/setvalue)-1)*100
-  setlingRise=s.setlingMax*0.9;
-
-  riseIdx=find(x>setlingrise,1,'first');
+  s.setlingMax=max(y);
+  s.setlingMin=min(y(s.peak:end));
+  s.overshoot=((s.setlingMax/setValue)-1)*100
+  setlingRise=setValue*0.9;
+  
+ 
+  riseIdx=find(y>setlingRise,1,'first');
   if riseIdx==1
     s.riseTime=t(1);
   else
@@ -15,8 +19,8 @@ function s=RCinfo(y,t)
     v2=y(riseIdx);
     s.riseTime=t(riseIdx-1)+(t(riseIdx)-t(riseIdx-1))*((setlingRise-v1)/(v2-v1));
   end
-  setlingDelay=s.setlingMax*0.5;
-  delayIdx=find(x>setlingdelay,1,'first');
+  setlingDelay=setValue*0.5;
+  delayIdx=find(y>setlingDelay,1,'first');
    if delayIdx==1
     s.delayTime=t(1);
   else
---
+++ ANALYSIS CODE/OCTAVE/sinResp.m
@@ -0,0 +1,18 @@
+Function s=sinResp(u,y,t)
+
+
+%%% Gain
+s.ppGain=(max(y)-min(y))/(max(u)-min(u));
+% empirical sinusoid
+U=fft(u);
+gU=abs(U);
+pU=angle(U);
+Y=fft(y);
+gY=abs(Y);
+pY=angle(Y);
+pitch=find(gU==max(gU),1,'first');
+s.gain=gY(pitch)/gU(pitch);
+%%% Phaselag
+s.phase=pY(pitch)-pU(pitch);
+%%% power
+s.power=sum(gY.^2)/sum(gU.^2);
