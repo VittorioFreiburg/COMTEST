e1b4ac741cee1c790867ba82d6a71cf45e3a889b
VittorioLippi <vittorio.lippi@tu-berlin.de>
2021-10-25 13:21:58 +0200

    Memory fix
    20 ms sample time + shorter Sinusoidal sample csv

--- ANALYSIS/OCTAVE/test_data/input/extractTest.m
+++ ANALYSIS/OCTAVE/test_data/input/extractTest.m
@@ -8,6 +8,16 @@ UBx = DATA(:,9) - LBx;
 COMx = LBx + 1/4*(UBx);
 hCOM = (hh + 1/4*(hs-hh))*100;
 angleCOM = 180/pi*asin((COMx)/hCOM);
-TIME=(1:length(angleCOM))*0.01;
+TIME=(1:length(angleCOM))*0.02;
 
 dlmwrite(OUT,[TIME',angleCOM]);
+
+
+
+%PRTSinfo(TIME',angleCOM')
+
+outSin='SinSample.csv';
+tilt=0.8*sin(TIME');
+sway=0.5*sin(TIME'-0.8)+0.1*sin(TIME'./7.4);
+dlmwrite(outSin,[TIME',sway,tilt]);
+plot(TIME,[sway,tilt]);
